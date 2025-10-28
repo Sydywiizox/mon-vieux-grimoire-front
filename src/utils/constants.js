97025dbe79dea863e64c375492d36a5637f48ae7
Sydywiizox <andrea_duhamel@outlook.fr>
2025-10-28 23:53:53 +0100

    Update API URL to new production endpoint

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const API_URL = "https://mon-vieux-grimoire-6kf7.onrender.com";
+const API_URL = "https://convincing-alyson-sydywiizox-1504e52b.koyeb.app";
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/signup`,
   SIGN_IN: `${API_URL}/api/auth/login`,
